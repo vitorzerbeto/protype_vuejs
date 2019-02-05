f186880fb532ce8f779ae60120d0a28d147a12eb
Vitor Zerbeto <vitor.zerbeto@aptasd.com.br>
2019-02-05 08:40:20 -0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        message: "You loaded this page on " + new Date().toLocaleDateString()
+    },
+    methods: {
+        showMessage: function () {
+            this.message = this.message.split(' ').reverse().join(' ')
+        }
+    }
+});
