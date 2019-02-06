92b9a55ca62d4d7af587915fe5b3cd688939a972
Vitor Zerbeto <vitor.zerbeto@aptasd.com.br>
2019-02-06 08:19:46 -0200

    Usando v-model, v-for e v-if

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const app = new Vue({
     el: "#app",
     data: {
+        seen: true,
+        array: [
+            {text: 'Obj01'},
+            {text: 'Obj02'},
+            {text: 'Obj03'}
+        ],
         message: "You loaded this page on " + new Date().toLocaleDateString()
     },
     methods: {
